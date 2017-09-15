d498ba6497459d296e94dfab6d274ab0131d28c3
Jacek Golebiowski <jgolebiowski@Jaceks-MacBook-Air.local>
2017-09-15 15:53:17 +0200

    prepping hpc run

--- controlTrainRNN.py
+++ controlTrainRNN.py
@@ -7,21 +7,21 @@ simulationIndex = 0
 # simulationIndex = int(sys.argv[1])
 
 
-# pickleFilename = "dataSet/trump_campaign.pkl"
-pickleFilename = "dataSet/singleSentence.pkl"
+pickleFilename = "dataSet/trump_campaign.pkl"
+# pickleFilename = "dataSet/singleSentence.pkl"
 with open(pickleFilename, "rb") as fp:
     x, index_to_word, word_to_index = pickle.load(fp)
 
 seriesLength, nFeatures = x.shape
-# nExamples = simulationIndex
-# exampleLength = 20
-# nHidden = 80
-# nHidden2 = 100
+nExamples = simulationIndex
+exampleLength = 20
+nHidden = 80
+nHidden2 = 100
 
-nExamples = 2
-exampleLength = 15
-nHidden = 35
-nHidden2 = 40
+# nExamples = 2
+# exampleLength = 15
+# nHidden = 35
+# nHidden2 = 40
 
 mainGraph = ga.Graph(False)
 dummyX = np.zeros((nExamples, exampleLength, nFeatures))
@@ -60,6 +60,7 @@ def fprime(p, data, labels, costOperationsList=costOperationsList, mainGraph=mai
 
 param0 = mainGraph.unrollGradientParameters()
 print("Number of parameters to train:", len(param0))
+1/0
 adaGrad = ga.adaptiveSGDrecurrent(trainingData=x,
                                   param0=param0,
                                   epochs=1e3,
