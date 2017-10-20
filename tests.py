53d969d6b3b374aa82943d65e1a42a3a1a6748df
Jacek Golebiowski <jacek.golebiowski14@imperial.ac.uk>
2017-10-20 12:16:19 +0100

    less tests

--- tests.py
+++ tests.py
@@ -54,39 +54,39 @@ def test_trainDense():
     assert(testAcc > 0.75)
 
 
-def test_trainCNN():
-    pickleFilename = "dataSet/notMNIST_small.pkl"
-    with open(pickleFilename, "rb") as fp:
-        allDatasets = pickle.load(fp)
+# def test_trainCNN():
+#     pickleFilename = "dataSet/notMNIST_small.pkl"
+#     with open(pickleFilename, "rb") as fp:
+#         allDatasets = pickle.load(fp)
 
-    X = allDatasets["train_dataset"]
-    Y = allDatasets["train_labels"]
+#     X = allDatasets["train_dataset"]
+#     Y = allDatasets["train_labels"]
 
-    Xtest = allDatasets["test_dataset"]
-    Ytest = allDatasets["test_labels"]
+#     Xtest = allDatasets["test_dataset"]
+#     Ytest = allDatasets["test_labels"]
 
-    Xvalid = allDatasets["valid_dataset"]
-    Yvalid = allDatasets["valid_labels"]
+#     Xvalid = allDatasets["valid_dataset"]
+#     Yvalid = allDatasets["valid_labels"]
 
-    simulationIndex = 0
-    mainGraph = runTrainCNN(simulationIndex, X, Y)
+#     simulationIndex = 0
+#     mainGraph = runTrainCNN(simulationIndex, X, Y)
 
-    trainAcc = ga.calculateAccuracy(mainGraph, X, Y)
-    validAcc = ga.calculateAccuracy(mainGraph, Xvalid, Yvalid)
-    testAcc = ga.calculateAccuracy(mainGraph, Xtest, Ytest)
+#     trainAcc = ga.calculateAccuracy(mainGraph, X, Y)
+#     validAcc = ga.calculateAccuracy(mainGraph, Xvalid, Yvalid)
+#     testAcc = ga.calculateAccuracy(mainGraph, Xtest, Ytest)
 
-    assert(trainAcc > 0.9)
-    assert(validAcc > 0.75)
-    assert(testAcc > 0.75)
+#     assert(trainAcc > 0.9)
+#     assert(validAcc > 0.75)
+#     assert(testAcc > 0.75)
 
 
-def test_trainRNN():
-    # ------ This is a very limited dataset, load a lrger one for better results
-    pickleFilename = "dataSet/singleSentence.pkl"
-    with open(pickleFilename, "rb") as fp:
-        x, index_to_word, word_to_index = pickle.load(fp)
+# def test_trainRNN():
+#     # ------ This is a very limited dataset, load a lrger one for better results
+#     pickleFilename = "dataSet/singleSentence.pkl"
+#     with open(pickleFilename, "rb") as fp:
+#         x, index_to_word, word_to_index = pickle.load(fp)
 
-    simulationIndex = 0
-    mainGraph, cache, finalCost = runTrainRNN(simulationIndex, x)
+#     simulationIndex = 0
+#     mainGraph, cache, finalCost = runTrainRNN(simulationIndex, x)
 
-    assert(finalCost < 1e-3)
+#     assert(finalCost < 1e-3)
